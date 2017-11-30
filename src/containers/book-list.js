224963986f5ee575735b4568f378e242cd4c5567
uidevperson <uidevthing@gmail.com>
2017-11-30 21:35:53 +0000

    react-redux missing booklist

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default class BookList extends Component {
+class BookList extends Component {
 	renderList(){
 		this.props.books.map(book => {
 			return (
@@ -16,6 +17,16 @@ export default class BookList extends Component {
 			<ul className="list-group col-sm-4">
 				{this.renderList()}
 			</ul>
-		);
+		)
 	}
 }
+
+function mapStateToProps(state) {
+	// whatever is returned
+	// will show up as props inside booklist
+	return {
+		books: state.books
+	};
+}
+
+export default connect(mapStateToProps)(BookList);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,8 @@ import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
 
 const rootReducer = combineReducers({
-  books: BooksReducer
+  books: BooksReducer,
+  test: 'test'
 });
 
 export default rootReducer;
