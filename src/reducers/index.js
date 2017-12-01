856a7f9ed3f2fd4c1d1e67547c6e1a0918cf2b72
uidevperson <32483505+uidevperson@users.noreply.github.com>
2017-12-01 15:53:14 +0000

    consuming actions in reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,10 @@
 import { combineReducers } from 'redux';
 import BooksReducer from './reducer_books';
+import ActiveBook from './reducer_active_book';
 
 const rootReducer = combineReducers({
   books: BooksReducer,
-  test: {'test': 'test'}
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
